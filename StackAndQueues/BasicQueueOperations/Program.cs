939f8db242ed9b0ba4e820ffd91ad02d4abfd97a
Martin Milev <marto379@abv.bg>
2022-05-22 19:24:22 +0300

    add

---
+++ StackAndQueues/BasicQueueOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = numbers[0];
+            int S = numbers[1];
+            int X = numbers[2];
+            int[] digits = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                queue.Enqueue(digits[i]);
+            }
+            for (int i = 0; i < S; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(X))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = Console.ForegroundColor;
+                Console.WriteLine("true");
+               
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(queue.Min());
+            }
+
+
+        }
+    }
+}
---
+++ StackAndQueues/BasicStackOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = numbers[0];
+            int S = numbers[1];
+            int X = numbers[2];
+            int[] digits = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                stack.Push(digits[i]);
+            }
+            for (int i = 0; i < S; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (stack.Contains(X))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+
+
+        }
+    }
+}
