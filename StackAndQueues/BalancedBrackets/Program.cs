ea7fae5cab24b8648981319890a049e7a0c39230
Martin Milev <marto379@abv.bg>
2022-05-23 12:44:18 +0300

    Add

---
+++ StackAndQueues/BalancedBrackets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace BalacedBrackets;
+class Program {
+    static void Main(string[] args)
+    {
+        string brackets = Console.ReadLine();
+        Stack<char> stack = new Stack<char>();
+        bool areBalanced = false;
+
+        foreach (char brecket in brackets)
+        {
+            if (brecket == '{' || brecket == '[' || brecket == '(')
+            {
+                stack.Push(brecket);
+            }
+            else if (brecket == '}' || brecket == ']' || brecket == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    areBalanced = false;
+                    break;
+                }
+                
+                if (brecket == ')' && stack.Pop() == '(')
+                {
+                    areBalanced = true;
+                    
+                }
+                else if (brecket == ']' && stack.Pop() == '[')
+                {
+                    areBalanced = true;
+                }
+                else if (brecket == '}' && stack.Pop() == '{')
+                {
+                    areBalanced = true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        if (areBalanced)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
+
+ 
