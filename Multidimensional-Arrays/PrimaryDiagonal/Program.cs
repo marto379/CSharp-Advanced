c9edf0a5cf4d19909fa33b1ea3fe3c04e17733aa
Martin Milev <marto379@abv.bg>
2022-05-24 23:23:11 +0300

    Add

---
+++ Multidimensional-Arrays/PrimaryDiagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int rows = N;
+            int cows = N;
+            int[,] matrix = new int[rows,cows];
+            int sumDiagonal = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int cow = 0; cow < cows; cow++)
+                {
+                    matrix[row, cow] = arr[cow];
+                    if (row == cow)
+                    {
+                        sumDiagonal += arr[cow];
+                    }
+                }
+            }
+            Console.WriteLine(sumDiagonal);
+            
+        }
+    }
+}
---
+++ Multidimensional-Arrays/SymbolInMatrix/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int rows = N;
+            int cows = N;
+
+            char[,] matrix = new char[rows, cows];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] ch = Console.ReadLine().ToCharArray();
+                for (int cow = 0; cow < cows; cow++)
+                {
+                    matrix[row, cow] = ch[cow];
+                }
+            }
+            char chToFind = Console.ReadLine()[0];
+            for (int row = 0; row < rows; row++)
+            {
+                for (int cow = 0; cow < cows; cow++)
+                {
+                    if (matrix[row,cow] == chToFind)
+                    {
+                        Console.WriteLine($"({row}, {cow})");
+                        return;
+                    }
+                    
+                }
+            }
+            Console.WriteLine($"{chToFind} does not occur in the matrix");
+        }
+    }
+}
