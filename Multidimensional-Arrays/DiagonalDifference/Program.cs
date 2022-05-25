9ccbeddbb6ad40be06d0f9eee87ab920fcbf5af7
Martin Milev <marto379@abv.bg>
2022-05-25 15:32:24 +0300

    Add

---
+++ Multidimensional-Arrays/DiagonalDifference/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rows = n;
+            int cows = n;
+            int[,] matrix = new int[n, n];
+            int sumPrimary = 0;
+            int sumSecond = 0;
+
+            for (int row = 0; row < n ; row++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int cow = 0; cow < cows; cow++)
+                {
+                    matrix[row,cow] = arr[cow];
+                    if (row == cow)
+                    {
+                        sumPrimary += arr[cow];
+                    }
+                    if (row + cow == rows - 1)
+                    {
+                        sumSecond += arr[cow];
+                    }
+
+                }
+            }
+            Console.WriteLine(Math.Abs(sumPrimary - sumSecond));
+        }
+    }
+}
---
+++ Multidimensional-Arrays/SquaresInMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = nums[0];
+            int cows = nums[1];
+
+            string[,] matrix = new string[rows, cows]; ;
+            int counter = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] arr = Console.ReadLine().Split();
+                
+
+                for (int cow = 0; cow < cows; cow++)
+                {
+                    matrix[row, cow] = arr[cow];
+                    
+                }
+            }
+            for (int row = 0; row < rows -1; row++)
+            {
+                for (int cow = 0; cow < cows -1; cow++)
+                {
+                    if (matrix[row, cow] == matrix[row + 1, cow] && matrix[row, cow] == matrix[row, cow + 1] && matrix[row, cow] == matrix[row + 1, cow + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+            
+        }
+    }
+}
