18b5c938a7159efda3e37c9e6ed27e0873706dfe
Martin Milev <marto379@abv.bg>
2022-05-26 16:31:04 +0300

    Add

---
+++ Multidimensional-Arrays/SnakeMoves/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = coordinates[0];
+            int cols = coordinates[1];
+            char[,] matrix = new char[rows, cols];
+
+            string input = Console.ReadLine();
+            var queue = new Queue<char>(input);
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = queue.Peek(); 
+                        queue.Enqueue(queue.Dequeue());
+
+                    }
+                }
+                else
+                {
+                    for (int col = cols-1; col >= 0; col--)
+                    {
+                        matrix[row, col] = queue.Peek();
+                        queue.Enqueue(queue.Dequeue());
+                    }
+                }
+                
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
