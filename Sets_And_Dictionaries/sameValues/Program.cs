f94e02f7a6c9c3c9e502d29fe92b43994ab9ed1f
Martin Milev <marto379@abv.bg>
2022-05-29 22:35:03 +0300

    Add

---
+++ Sets_And_Dictionaries/sameValues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sameValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double,int> values = new Dictionary<double, int>();
+            double[] arr = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                double currNum = arr[i];
+                if (!values.ContainsKey(currNum))
+                {
+                    values.Add(currNum,0);
+                }
+                values[currNum]++;
+            }
+            foreach (var item in values)
+            {
+                double num = item.Key;
+                int count = item.Value;
+                Console.WriteLine($"{num} - {count} times");
+            }
+
+        }
+    }
+}
