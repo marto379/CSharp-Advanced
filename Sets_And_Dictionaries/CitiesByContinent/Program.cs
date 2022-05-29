4b1592d82842ad0d5df6acfd2f392751f11f28a7
Martin Milev <marto379@abv.bg>
2022-05-30 00:18:30 +0300

    Add

---
+++ Sets_And_Dictionaries/CitiesByContinent/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CitiesByContinent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string,Dictionary<string,List<string>>> dic = new Dictionary<string,Dictionary<string,List<string>>>();  
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!dic.ContainsKey(continent))
+                {
+                    dic.Add(continent, new Dictionary<string,List<string>>());
+                }
+                
+                if (!dic[continent].ContainsKey(country))
+                {
+                    dic[continent].Add(country, new List<string>());
+                }
+                dic[continent][country].Add(city);
+                
+            }
+            foreach (var item in dic)
+            {
+                Console.WriteLine(item.Key + ":");
+                foreach (var county in item.Value)
+                {
+                    string ctr = county.Key;
+                    List<string> cities = county.Value;
+                    Console.WriteLine($"    {ctr} -> {string.Join(", ",cities)}");
+                }
+            }
+        }
+    }
+}
