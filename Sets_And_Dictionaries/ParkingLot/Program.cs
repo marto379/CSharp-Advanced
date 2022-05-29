043fee51f2b2cccb64608819b59ca370fc601b7f
Martin Milev <marto379@abv.bg>
2022-05-30 00:48:16 +0300

    Add

---
+++ Sets_And_Dictionaries/ParkingLot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            HashSet<string> set = new HashSet<string>();    
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] command = input.Split(", ");
+                string inOrOut = command[0];    
+                string registracion = command[1];
+                if (inOrOut == "IN")
+                {
+                    set.Add(registracion);
+                }
+                else
+                {
+                    set.Remove(registracion);
+                }
+            }
+            if (set.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine,set));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
