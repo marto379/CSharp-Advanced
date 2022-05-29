77f15fe6beea681699ba44b00c0b90071c52e2e0
Martin Milev <marto379@abv.bg>
2022-05-29 23:05:51 +0300

    Add

---
+++ Sets_And_Dictionaries/AverageStudentGrades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> dict = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                double grade = double.Parse(input[1]);
+
+                if (!dict.ContainsKey(name))
+                {
+                    dict.Add(name, new List<double>());
+                }
+                dict[name].Add(grade);
+
+            }
+
+            foreach (var item in dict)
+            {
+                string grades = string.Join(" ",item.Value.Select(x => x.ToString("f2")));
+                Console.WriteLine($"{item.Key} -> {grades} (avg: {item.Value.Average():f2})");
+            }
+        }
+    }
+}
