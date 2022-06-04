9eb88b6c6f024990b58dad84ad3710ef3edb5e3e
Martin Milev <marto379@abv.bg>
2022-06-04 11:15:06 +0300

    Add

---
+++ FunctionalProgramming/SortEvenNums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split(", ")
+                .Select(n => int.Parse(n))
+                .Where(n => n % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(String.Join(", ",tokens));
+        }
+    }
+}
