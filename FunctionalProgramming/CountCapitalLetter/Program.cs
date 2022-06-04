734db2ab812b6bea6aa3c8a45fc755238b426984
Martin Milev <marto379@abv.bg>
2022-06-04 12:57:57 +0300

    Add

---
+++ FunctionalProgramming/CountCapitalLetter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace CountCapitalLetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join("\n", Console.ReadLine().Split().Where(x => char.IsUpper(x[0]) && x.Length > 0)));
+        }
+    }
+}
---
+++ FunctionalProgramming/SortNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace SortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string,int> parse = (string str) => int.Parse(str);
+            string tokens = Console.ReadLine();
+            int[] nums = tokens.Split(", ").Select(parse).ToArray();
+            Console.WriteLine(nums.Count() + "\n" + nums.Sum());
+            
+        }
+    }
+}
