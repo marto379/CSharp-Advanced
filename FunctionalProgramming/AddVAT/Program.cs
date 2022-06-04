957d74f04b45a1fd0266fa99c0d0b89f1518cca1
Martin Milev <marto379@abv.bg>
2022-06-04 14:48:44 +0300

    Add

---
+++ FunctionalProgramming/AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal> addVAT = x => x *= 1.2m;
+
+            string tokens = Console.ReadLine();
+            decimal[] nums = tokens.Split(", ").Select(decimal.Parse).Select(addVAT).ToArray();
+            Array.ForEach(nums, x => Console.WriteLine("{0:f2}",x));
+        }
+    }
+}
