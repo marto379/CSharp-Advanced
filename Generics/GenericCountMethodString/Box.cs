256ab66beb7738d43ad4bc6b41694904b98717a4
Martin Milev <marto379@abv.bg>
2022-06-15 18:36:57 +0300

    add

---
+++ Generics/GenericCountMethodString/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodString
+{
+    internal class Box<T> : IComparable<T> where T: IComparable<T> 
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public Box(List<T> elements)
+        {
+            Elements = elements;
+        }
+
+        public List<T> Elements { get;}
+        public T Element { get;}
+
+        
+
+        public int CountOfGreaterElement<T>(List<T> list, T readline) where T: IComparable =>
+            list.Count(word => word.CompareTo(readline) > 0);
+
+        public int CompareTo(T other)
+            => Element.CompareTo(other);
+
+    }
+}
---
+++ Generics/GenericCountMethodString/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numLines = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+
+            for (int i = 0; i < numLines; i++)
+            {
+                var input = Console.ReadLine();
+                list.Add(input);
+
+            }
+            var box = new Box<string>(list);
+            var elementToCompare = Console.ReadLine();
+            var count = box.CountOfGreaterElement(list, elementToCompare);
+            Console.WriteLine(count);
+        }
+    }
+}
