9e9715ce8c659e844b4ef5929aea5afa64596b4e
Martin Milev <marto379@abv.bg>
2022-06-15 15:32:26 +0300

    Add

---
+++ Generics/GenericBoxOfString/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    internal class Box<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public T Element { get;}
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ Generics/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                var box = new Box<int>(input);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
